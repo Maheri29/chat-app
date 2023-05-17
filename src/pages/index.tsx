abaabd195486dd02f26375a0c3ff4bfae8bfc88c
Maheri29 <hei.mahery.2@gmail.com>
2023-05-17 18:02:42 +0300

    feat: update sign up form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,20 +6,28 @@ type FormData = {
   nom: string;
   prenom: string;
   motDePasse: string;
+  confirmMotDePasse: string;
   email: string;
 };
 
 function RegistrationForm() {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch // Utilisez la fonction watch pour surveiller les valeurs des champs
+  } = useForm<FormData>();
 
   const onSubmit = (data: FormData) => {
     console.log(data);
   };
 
+  const password = watch('motDePasse'); // Récupère la valeur du champ "motDePasse"
+
   return (
     <form className={styles['registration-form']} onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <label>Nom</label>
+        <label>Pseudo</label>
         <input
           type="text"
           {...register('nom', { required: true })}
@@ -27,23 +35,7 @@ function RegistrationForm() {
         {errors.nom && <span>Enter your last name here please</span>}
       </div>
 
-      <div>
-        <label>Prénom</label>
-        <input
-          type="text"
-          {...register('prenom', { required: true })}
-        />
-        {errors.prenom && <span>Enter your first name here please </span>}
-      </div>
 
-      <div>
-        <label>Mot de passe</label>
-        <input
-          type="password"
-          {...register('motDePasse', { required: true })}
-        />
-        {errors.motDePasse && <span>Enter a password here please</span>}
-      </div>
 
       <div>
         <label>Email</label>
@@ -55,6 +47,27 @@ function RegistrationForm() {
           })}
         />
         {errors.email && <span>Invalid mail adress</span>}
+      </div>
+
+      <div>
+        <label>Mot de passe</label>
+        <input
+          type="password"
+          {...register('motDePasse', { required: true })}
+        />
+        {errors.motDePasse && <span>Enter a password here please</span>}
+      </div>
+ 
+      <div>
+        <label>Confirmer le mot de passe</label>
+        <input
+          type="password"
+          {...register('confirmMotDePasse', {
+            required: true,
+            validate: (value) => value === password // Vérifie si la valeur correspond à celle du champ "motDePasse"
+          })}
+        />
+        {errors.confirmMotDePasse && <span>Les mots de passe ne correspondent pas</span>}
       </div>
 
       <input type="submit" value="S'inscrire" />
