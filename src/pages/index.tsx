141ea328cfbf4cebdbd7d95cd992e8a97b2bea2b
Maheri29 <hei.mahery.2@gmail.com>
2023-05-18 12:54:01 +0300

    feat: update sign up with yup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 import styles from '../styles/Home.module.css';
 
 type FormData = {
@@ -10,64 +11,59 @@ type FormData = {
   email: string;
 };
 
+const schema = yup.object().shape({
+  nom: yup.string().required('Le nom est requis'),
+  prenom: yup.string().required('Le prénom est requis'),
+  motDePasse: yup.string().required('Le mot de passe est requis'),
+  confirmMotDePasse: yup
+    .string()
+    .oneOf([yup.ref('motDePasse')], 'Les mots de passe ne correspondent pas')
+    .required('La confirmation du mot de passe est requise'),
+  email: yup.string().required("L'email est requis").email("L'email n'est pas valide"),
+});
+
 function RegistrationForm() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-    watch // Utilisez la fonction watch pour surveiller les valeurs des champs
+    watch,
   } = useForm<FormData>();
 
   const onSubmit = (data: FormData) => {
     console.log(data);
   };
 
-  const password = watch('motDePasse'); // Récupère la valeur du champ "motDePasse"
+  const password = watch('motDePasse');
+
+  const validatePasswordMatch = (value: string) => {
+    return value === password || 'Les mots de passe ne correspondent pas';
+  };
 
   return (
     <form className={styles['registration-form']} onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label>Pseudo</label>
-        <input
-          type="text"
-          {...register('nom', { required: true })}
-        />
-        {errors.nom && <span>Enter your last name here please</span>}
+        <input type="text" {...register('nom')} />
+        {errors.nom && <span>{errors.nom.message}</span>}
       </div>
-
-
 
       <div>
         <label>Email</label>
-        <input
-          type="email"
-          {...register('email', {
-            required: true,
-            pattern: /^\S+@\S+$/i
-          })}
-        />
-        {errors.email && <span>Invalid mail adress</span>}
+        <input type="email" {...register('email')} />
+        {errors.email && <span>{errors.email.message}</span>}
       </div>
 
       <div>
         <label>Mot de passe</label>
-        <input
-          type="password"
-          {...register('motDePasse', { required: true })}
-        />
-        {errors.motDePasse && <span>Enter a password here please</span>}
+        <input type="password" {...register('motDePasse')} />
+        {errors.motDePasse && <span>{errors.motDePasse.message}</span>}
       </div>
- 
+
       <div>
         <label>Confirmer le mot de passe</label>
-        <input
-          type="password"
-          {...register('confirmMotDePasse', {
-            required: true,
-            validate: (value) => value === password // Vérifie si la valeur correspond à celle du champ "motDePasse"
-          })}
-        />
-        {errors.confirmMotDePasse && <span>Les mots de passe ne correspondent pas</span>}
+        <input type="password" {...register('confirmMotDePasse', { validate: validatePasswordMatch })} />
+        {errors.confirmMotDePasse && <span>{errors.confirmMotDePasse.message}</span>}
       </div>
 
       <input type="submit" value="S'inscrire" />
